1fc1b7381d58d19995ea8682dfdde6e527d4fa0a
sepehrnarimani <69726077+sepehrnarimani@users.noreply.github.com>
2025-12-27 15:54:12 +0330

    added sidebar and navbar to setting page.

---
+++ src/app/components/settings-sidebar/settings-sidebar.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+
+@Component({
+  selector: 'app-settings-sidebar',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './settings-sidebar.html',
+  styleUrl: './settings-sidebar.css',
+})
+export class SettingsSidebar {
+
+}
--- src/app/pages/settings/profile/profile.ts
+++ src/app/pages/settings/profile/profile.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SettingsSidebar } from "../../../components/settings-sidebar/settings-sidebar";
+import { NavbarComponent } from "../../../components/navbar/navbar";
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [SettingsSidebar, NavbarComponent],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
